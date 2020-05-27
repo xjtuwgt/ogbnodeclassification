423d6a4cc24a69bb80deac4e33d8726adb9ffff3
guangtao.wang <guangtao.wang@jd.com>
2020-05-27 09:15:49 -0700

    initial

---
+++ ogb_arxiv_tuner_jobs.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+JOBS_PATH=ogbn_arxiv_jobs
+LOGS_PATH=logs
+for ENTRY in "${JOBS_PATH}"/*.sh; do
+  chmod +x $ENTRY
+  FILE_NAME="$(basename "$ENTRY")"
+  echo $FILE_NAME
+  /mnt/cephfs2/asr/users/ming.tu/software/kaldi/egs/wsj/s5/utils/queue.pl -q g.q -l gpu=2 $LOGS_PATH/$FILE_NAME.log $ENTRY &
+  sleep 3
+done
---
+++ runarxiv.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+
+python -u -c 'import torch; print(torch.__version__)'
+
+CODE_PATH=codes
+DATA_PATH=dataset
+SAVE_PATH=models
+
+#The first four parameters must be provided
+MODE=$1
+DATASET=$2
+SAVE_ID=$3
+SAVE=$SAVE_PATH/"$DATASET"_"$SAVE_ID"
+
+#Only used in training
+LAYERS=$4
+HIDDEN_DIM=$5
+PROJECT_DIM=$6
+HEADS=$7
+HOPS=$8
+LEARNING_RATE=${9}
+FEA_DROP=${10}
+ATT_DROP=${11}
+NEG_SLOP=${12}
+WEIGHTDECAY=${13}
+EPOCHS=${14}
+ALPHA=${15}
+TOPK=${16}
+TOPKTYPE=${17}
+EDROP=${18}
+LOOP=${19}
+SEED=${20}
+
+
+if [ $MODE == "train" ]
+then
+
+echo "Start Training......"
+
+/mnt/cephfs2/asr/users/ming.tu/sgetools/run_gpu.sh python -u $CODE_PATH/trainarxiv.py --do_train \
+    --cuda \
+    --data_path $DATA_PATH\
+    --dataset $DATASET\
+    --save_path $SAVE\
+    --hop_num $HOPS\
+    --epochs $EPOCHS\
+    --num_heads $HEADS\
+    --num_layers $LAYERS\
+    --num_hidden $HIDDEN_DIM\
+    --in_drop $FEA_DROP\
+    --attn_drop $ATT_DROP\
+    --alpha $ALPHA\
+    --project_dim $PROJECT_DIM\
+    --lr $LEARNING_RATE\
+    --negative_slope $NEG_SLOP\
+    --top_k $TOPK\
+    --weight_decay $WEIGHTDECAY\
+    --topk_type $TOPKTYPE\
+    --edge_drop $EDROP\
+    --self_loop $LOOP\
+    --seed $SEED
+
+else
+   echo "Unknown MODE" $MODE
+fi

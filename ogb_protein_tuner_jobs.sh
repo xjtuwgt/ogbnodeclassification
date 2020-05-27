e3ed8f6564015e812e84d2b4c9dafb21678295cb
guangtao.wang <guangtao.wang@jd.com>
2020-05-27 09:54:30 -0700

    initial

--- ogb_protein_tuner_jobs.sh
+++ ogb_protein_tuner_jobs.sh
@@ -5,6 +5,6 @@ for ENTRY in "${JOBS_PATH}"/*.sh; do
   chmod +x $ENTRY
   FILE_NAME="$(basename "$ENTRY")"
   echo $FILE_NAME
-  queue.pl -q g.q -l gpu=2 $LOGS_PATH/$FILE_NAME.log $ENTRY &
+  /home/snap/Documents/guangtaowang/GraphDiffusionForOGB/ogbnodeclassification/queue.pl -q g.q -l gpu=2 $LOGS_PATH/$FILE_NAME.log $ENTRY &
   sleep 3
 done

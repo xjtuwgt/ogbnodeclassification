de1a7b2b21f573783df7226436d2727f1ed9e5cc
guangtao.wang <guangtao.wang@jd.com>
2020-05-27 09:48:23 -0700

    initial

--- ogb_arxiv_tuner_jobs.sh
+++ ogb_arxiv_tuner_jobs.sh
@@ -5,6 +5,6 @@ for ENTRY in "${JOBS_PATH}"/*.sh; do
   chmod +x $ENTRY
   FILE_NAME="$(basename "$ENTRY")"
   echo $FILE_NAME
-  /mnt/cephfs2/asr/users/ming.tu/software/kaldi/egs/wsj/s5/utils/queue.pl -q g.q -l gpu=2 $LOGS_PATH/$FILE_NAME.log $ENTRY &
+  queue.pl -q g.q -l gpu=2 $LOGS_PATH/$FILE_NAME.log $ENTRY &
   sleep 3
 done

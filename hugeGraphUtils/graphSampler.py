6668993d83f8d010b6d0a3665229afc4c9b673b6
guangtao.wang <guangtao.wang@jd.com>
2020-05-27 21:31:01 -0700

    initial

--- hugeGraphUtils/graphSampler.py
+++ hugeGraphUtils/graphSampler.py
@@ -18,10 +18,9 @@ def graph_k_neighbor_sampler(graph: DGLGraph, knn: int):
         if neighbor_num <= knn:
             sample_knns[:, :neighbor_num] = node_ids
         else:
-            r = torch.randperm(sample_num)
-            c = torch.randperm(neighbor_num)
-            perm_node_ids = node_ids[r[:, None], c]
-            sample_knns[:, :knn] = perm_node_ids[:, :knn]
+            for i in range(sample_num):
+                node_ids[i] = node_ids[i][torch.randperm(neighbor_num)]
+            sample_knns[:, :knn] = node_ids[:, :knn]
         return {'knn_ids': sample_knns}
 
     graph.register_reduce_func(knn_sampler_reduce_func)
@@ -34,6 +33,7 @@ def graph_k_neighbor_sampler(graph: DGLGraph, knn: int):
     edge_pairs = edge_pairs.view(graph.number_of_nodes() * knn, -1)
     # print(edge_pairs.shape)
     edge_pairs = edge_pairs[edge_pairs[:,0]>=0, :]
+    print(edge_pairs.shape)
     # print(nids.shape, knnids.shape, edge_pairs.shape, kcopyids.shape)
     new_graph = graph_construction(graph=graph, edge_pairs=edge_pairs)
     print('Sampling takes {}'.format(time() - start_time))
@@ -58,14 +58,13 @@ def graph_ratio_neighbor_sampler(graph: DGLGraph, sample_ratio: float=0.1):
         node_ids = nodes.mailbox['m_src_id']
         node_ids = node_ids.squeeze(dim=-1)
         sample_num, neighbor_num = node_ids.shape[0], node_ids.shape[1]
-        sample_knns = -torch.ones((node_ids.shape[0], knn), dtype=torch.long)
+        sample_knns = -torch.ones((node_ids.shape[0], knn), dtype=torch.long).to(node_ids.device)
         if neighbor_num <= knn:
             sample_knns[:, :neighbor_num] = node_ids
         else:
-            r = torch.randperm(sample_num)
-            c = torch.randperm(neighbor_num)
-            perm_node_ids = node_ids[r[:, None], c]
-            sample_knns[:, :knn] = perm_node_ids[:, :knn]
+            for i in range(sample_num):
+                node_ids[i] = node_ids[i][torch.randperm(neighbor_num)]
+            sample_knns[:, :knn] = node_ids[:, :knn]
         return {'knn_ids': sample_knns}
 
     graph.register_reduce_func(knn_sampler_reduce_func)
@@ -82,11 +81,16 @@ def graph_ratio_neighbor_sampler(graph: DGLGraph, sample_ratio: float=0.1):
     return new_graph
 
 def graph_construction(graph: DGLGraph, edge_pairs):
+    graph = graph.local_var()
     new_graph = DGLGraph()
     num_nodes = graph.number_of_nodes()
     new_graph.add_nodes(num_nodes)
     src_nodes, dest_nodes = edge_pairs[:,0], edge_pairs[:,1]
+    edge_ids = graph.edge_ids(src_nodes, dest_nodes)
+    # print('here', edge_ids.shape, src_nodes.shape)
     new_graph.add_edges(src_nodes, dest_nodes)
     for key, value in graph.ndata.items():
         new_graph.ndata[key] = value
+    for key, value in graph.edata.items():
+        new_graph.edata[key] = value[edge_ids]
     return new_graph
